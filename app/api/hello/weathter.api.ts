8aba92426056af44f5ce02bc4db34e32d11ee76b
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-02-12 15:19:33 +0500

    final changes made

--- app/api/hello/weathter.api.ts
+++ app/api/hello/weathter.api.ts
@@ -1,10 +1,14 @@
-const API_KEY = "62da8bc718ef47af89494515251202";
-const BASE_URL = "http://api.weatherapi.com/v1/current.json";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = "http://api.weatherapi.com/v1";
 
 export const fetchWeather = async (city: string) => {
   try {
-    const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${city}`);
-    if (!response.ok) throw new Error("Failed to fetch weather data");
+    const response = await fetch(
+      `${BASE_URL}/current.json?key=${API_KEY}&q=${city}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
     return await response.json();
   } catch (error) {
     console.error(error);
